53165292fdebc78cc838adf5fe699ef009fd0101
Nil <nilbadia@gmail.com>
2025-11-24 21:48:18 +0100

    Exercise 8 but on steroids

--- Assets/Ejercicios/E.08/scripts/multiplyScript.cs
+++ Assets/Ejercicios/E.08/scripts/multiplyScript.cs
@@ -7,19 +7,10 @@ public class multiplyScript : MonoBehaviour
     void Start()
     {
 
-        float[] tableNumbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-
-        Debug.Log(number * tableNumbers[0]);
-        Debug.Log(number * tableNumbers[1]);
-        Debug.Log(number * tableNumbers[2]);
-        Debug.Log(number * tableNumbers[3]);
-        Debug.Log(number * tableNumbers[4]);
-        Debug.Log(number * tableNumbers[5]);
-        Debug.Log(number * tableNumbers[6]);
-        Debug.Log(number * tableNumbers[7]);
-        Debug.Log(number * tableNumbers[8]);
-        Debug.Log(number * tableNumbers[9]);
-
+        for (int i = 1; i < 11; i++)
+        {
+            Debug.Log(number * i);
+        }
 
     }
 
