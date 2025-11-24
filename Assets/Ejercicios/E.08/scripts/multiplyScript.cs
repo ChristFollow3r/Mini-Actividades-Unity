29002ccbecdf4c942d8d655bb0605537c3501494
Nil <nilbadia@gmail.com>
2025-11-24 21:41:23 +0100

    Fixed exercise 8

--- Assets/Ejercicios/E.08/scripts/multiplyScript.cs
+++ Assets/Ejercicios/E.08/scripts/multiplyScript.cs
@@ -10,6 +10,7 @@ public class multiplyScript : MonoBehaviour
         float[] tableNumbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
         Debug.Log(number * tableNumbers[0]);
+        Debug.Log(number * tableNumbers[1]);
         Debug.Log(number * tableNumbers[2]);
         Debug.Log(number * tableNumbers[3]);
         Debug.Log(number * tableNumbers[4]);
