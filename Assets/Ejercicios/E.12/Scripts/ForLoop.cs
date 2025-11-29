8ee1198ffd5455ba6100e0db9a345f891f7928e2
Nil <nilbadia@gmail.com>
2025-11-29 16:36:43 +0100

    Exercise 12 done

---
+++ Assets/Ejercicios/E.12/Scripts/ForLoop.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class ForLoop : MonoBehaviour
+{
+    void Start()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Debug.Log(i + 1);
+        }
+    }
+
+    void Update()
+    {
+
+    }
+}
