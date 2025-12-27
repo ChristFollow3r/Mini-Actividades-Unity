40b425600c042e164bb4c357fc645fd7c0ed7d84
Nil <nilbadia@gmail.com>
2025-12-27 13:25:28 +0100

    I think i just added a space in the code

--- Assets/Ejercicios/E.21/Scripts/BruhStalker.cs
+++ Assets/Ejercicios/E.21/Scripts/BruhStalker.cs
@@ -5,12 +5,6 @@ public class BruhStalker : MonoBehaviour
     [SerializeField] private Rigidbody2D square;
     [SerializeField] private Rigidbody2D stalker;
 
-    void Start()
-    {
-
-    }
-
-
     void Update()
     {
         Vector3 stalkerPosition = square.transform.position - new Vector3(-1.5f, 0, 0);
