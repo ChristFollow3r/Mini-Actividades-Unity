db8dc3af5b14865b64b079a269743b67a5e8db8b
Nil <nilbadia@gmail.com>
2025-12-27 11:46:48 +0100

    Exercise 19 done using the new input system

---
+++ Assets/Ejercicios/E.19/Scripts/Movement.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float speed = 7f;
+    [SerializeField] private Rigidbody2D square;
+    private InputSystem_Actions inputSystem_actions;
+
+    private void Awake()
+    {
+        inputSystem_actions = new InputSystem_Actions();
+        inputSystem_actions.Player.Enable();
+    }
+
+    void Update()
+    {
+        square.transform.Translate(GetMovement() * speed * Time.deltaTime);
+        Debug.Log(GetMovement());
+    }
+
+    Vector2 GetMovement()
+    {
+        Vector2 movement = inputSystem_actions.Player.Move.ReadValue<Vector2>();
+        return movement;
+    }
+}
