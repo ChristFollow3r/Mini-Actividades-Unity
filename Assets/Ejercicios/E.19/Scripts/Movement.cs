f167b92d1ff36814c0a06f31219b857c26ab0777
Nil <nilbadia@gmail.com>
2025-12-27 12:01:29 +0100

    Added a comment

--- Assets/Ejercicios/E.19/Scripts/Movement.cs
+++ Assets/Ejercicios/E.19/Scripts/Movement.cs
@@ -5,7 +5,6 @@ public class Movement : MonoBehaviour
     [SerializeField] private float speed = 7f;
     [SerializeField] private Rigidbody2D square;
     private InputSystem_Actions inputSystem_actions;
-
     private void Awake()
     {
         inputSystem_actions = new InputSystem_Actions();
@@ -24,3 +23,5 @@ public class Movement : MonoBehaviour
         return movement;
     }
 }
+
+// I didnt use the position method but I think this is way better.
