f18a05b41ad8eb62235381a0f11b5d8b82913e87
Nil <nilbadia@gmail.com>
2025-12-27 10:31:25 +0100

    Exercise 16 done

---
+++ Assets/Ejercicios/E.16/Scripts/SimpleMovement.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class SimpleMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 3f;
+    [SerializeField] private Rigidbody2D square;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        square.transform.Translate(new Vector2(1, -1) * speed * Time.deltaTime);
+
+    }
+}
