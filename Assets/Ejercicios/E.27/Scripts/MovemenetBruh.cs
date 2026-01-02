648c6fcb3abb1bb4d6ca38db9fd3fe08e033e6a4
Nil <nilbadia@gmail.com>
2026-01-02 13:21:32 +0100

    fixed broken movement

--- Assets/Ejercicios/E.27/Scripts/MovemenetBruh.cs
+++ Assets/Ejercicios/E.27/Scripts/MovemenetBruh.cs
@@ -4,8 +4,6 @@ public class MovemenetBruh : MonoBehaviour
 {
     [SerializeField] private float speed = 7f;
     [SerializeField] private Rigidbody2D player;
-    float upDown;
-    float leftRight;
 
     // private InputSystem_Actions inputSystem_actions;
     // private void Awake()
@@ -16,6 +14,8 @@ public class MovemenetBruh : MonoBehaviour
 
     void Update()
     {
+        float upDown = 0f;
+        float leftRight = 0f;
 
 
         if (Input.GetKey(KeyCode.W)) upDown = 1f;
@@ -23,7 +23,7 @@ public class MovemenetBruh : MonoBehaviour
         if (Input.GetKey(KeyCode.A)) leftRight = -1f;
         if (Input.GetKey(KeyCode.D)) leftRight = 1f;
 
-        Vector2 input = new Vector2(upDown, leftRight);
+        Vector2 input = new Vector2(leftRight, upDown);
         Vector3 movement = new Vector3(input.x * speed * Time.deltaTime, input.y * speed * Time.deltaTime, 0f);
         player.transform.position += movement;
 
