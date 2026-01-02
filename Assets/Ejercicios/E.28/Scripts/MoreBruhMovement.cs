5efc143ac2e7eaf369eb966104cbd5e8e6de6e43
Nil <nilbadia@gmail.com>
2026-01-02 15:56:43 +0100

    Exercise 28 done

---
+++ Assets/Ejercicios/E.28/Scripts/MoreBruhMovement.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class MoreBruhMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 7f;
+    [SerializeField] private Rigidbody2D square;
+    private InputSystem_Actions inputSystem_actions;
+    private void Awake()
+    {
+        inputSystem_actions = new InputSystem_Actions();
+        inputSystem_actions.Player.Enable();
+    }
+
+    void FixedUpdate()
+    {
+        square.transform.Translate(GetMovement() * speed * Time.deltaTime);
+    }
+
+    Vector2 GetMovement()
+    {
+        Vector2 movement = inputSystem_actions.Player.Move.ReadValue<Vector2>();
+        return movement;
+    }
+}
+
