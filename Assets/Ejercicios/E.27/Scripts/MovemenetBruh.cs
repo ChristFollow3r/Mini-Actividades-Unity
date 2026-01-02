7e5c306fe59d0e72932e966dcc4256c322e1388b
Nil <nilbadia@gmail.com>
2026-01-02 13:18:14 +0100

    cleaned it up a little

--- Assets/Ejercicios/E.27/Scripts/MovemenetBruh.cs
+++ Assets/Ejercicios/E.27/Scripts/MovemenetBruh.cs
@@ -4,6 +4,8 @@ public class MovemenetBruh : MonoBehaviour
 {
     [SerializeField] private float speed = 7f;
     [SerializeField] private Rigidbody2D player;
+    float upDown;
+    float leftRight;
 
     // private InputSystem_Actions inputSystem_actions;
     // private void Awake()
@@ -15,15 +17,13 @@ public class MovemenetBruh : MonoBehaviour
     void Update()
     {
 
-        float x = 0f;
-        float y = 0f;
 
-        if (Input.GetKey(KeyCode.W)) y = 1f;
-        if (Input.GetKey(KeyCode.S)) y = -1f;
-        if (Input.GetKey(KeyCode.A)) x = -1f;
-        if (Input.GetKey(KeyCode.D)) x = 1f;
+        if (Input.GetKey(KeyCode.W)) upDown = 1f;
+        if (Input.GetKey(KeyCode.S)) upDown = -1f;
+        if (Input.GetKey(KeyCode.A)) leftRight = -1f;
+        if (Input.GetKey(KeyCode.D)) leftRight = 1f;
 
-        Vector2 input = new Vector2(x, y);
+        Vector2 input = new Vector2(upDown, leftRight);
         Vector3 movement = new Vector3(input.x * speed * Time.deltaTime, input.y * speed * Time.deltaTime, 0f);
         player.transform.position += movement;
 
