f98604b58a020ad51c7d70fa1b1b504bb0cbd1c5
Nil <nilbadia@gmail.com>
2026-01-15 23:54:44 +0100

    exercise 32 done

--- Assets/Ejercicios/E.32/Scripts/tagTriggerScript.cs
+++ Assets/Ejercicios/E.32/Scripts/tagTriggerScript.cs
@@ -2,15 +2,40 @@ using UnityEngine;
 
 public class tagTriggerScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float speed = 7f;
+    [SerializeField] private Rigidbody2D square;
+
+    private InputSystem_Actions inputSystem_actions;
+
+    private void Awake()
     {
-        
+        inputSystem_actions = new InputSystem_Actions();
+        inputSystem_actions.Player.Enable();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Vector2 movement = square.position + GetMovement() * speed * Time.fixedDeltaTime;
+        square.MovePosition(movement);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Debug.Log("Trigger working");
+        if (collision.CompareTag("square1")) Debug.Log("Dude entered square 1");
+        else Debug.Log("Dude entered square 2");
+
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        // if (square.CompareTag("Square1")) Debug.Log("Dude entered square 1");
+        // else Debug.Log("Dude entered square two");
+    }
+
+    Vector2 GetMovement()
+    {
+        Vector2 movement = inputSystem_actions.Player.Move.ReadValue<Vector2>();
+        return movement;
     }
 }
