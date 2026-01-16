8d41621db1a090ac32a5f9752be1cedd3af270f8
Nil <nilbadia@gmail.com>
2026-01-16 18:32:58 +0100

    fuck it i used cinemachine

--- Assets/Ejercicios/E.35/Scripts/ImDumbAndIDidntRead.cs
+++ Assets/Ejercicios/E.35/Scripts/ImDumbAndIDidntRead.cs
@@ -1,15 +1,16 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class ImDumbAndIDidntRead : MonoBehaviour
 {
-    [SerializeField] private Rigidbody2D player;
-    [SerializeField] private float cameraSpeed = 5f;
+    // [SerializeField] private Rigidbody2D player;
+    // [SerializeField] private Camera camera;
+    // [SerializeField] private float cameraSpeed = 5f;
 
 
 
-    void Update()
-    {
-        transform.position = Vector3.MoveTowards(this.transform.position, player.transform.position, cameraSpeed);
-    }
+    // void Update()
+    // {
+    //     camera.transform.position = Vector3.MoveTowards(this.transform.position, player.transform.position, cameraSpeed * Time.deltaTime);
+    //     camera.transform.position += new Vector3(0, 0, 10);
+    // }
 }
