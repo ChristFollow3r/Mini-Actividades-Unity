23e4abf451ab311732ecbe1a7a16ebf702a31485
Nil <nilbadia@gmail.com>
2026-01-16 19:03:43 +0100

    Exercise 38 done

---
+++ Assets/Ejercicios/E.38/Scripts/ButtonScripts.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScripts : MonoBehaviour
+{
+    public void ClickedButton()
+    {
+        Debug.Log("Im a button");
+    }
+
+
+}
